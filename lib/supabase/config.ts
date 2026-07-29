820b6c8562f7aa321e175966beed3b14073b28c2
suhail-home <suhailm9055@gmail.com>
2026-07-29 18:02:27 +0530

    env done

--- lib/supabase/config.ts
+++ lib/supabase/config.ts
@@ -2,8 +2,6 @@ export function getSupabaseCredentials() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const key = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
-  console.log("SUPABASE URL:", url);
-  console.log("SUPABASE KEY:", key);
 
   return {
     url: url!,
